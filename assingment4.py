8ce5bd98f25c1387553ae0db3f11da56468ad4cf
rustam-0701 <maskerushi0206@gmail.com>
2026-07-31 05:15:14 +0530

    Add files via upload

---
+++ assingment4.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n ==1:
+        return 1
+
+    a, b = 0, 1
+
+    for i in range(2, n+1):
+        a, b = b, a+b
+
+    return b
+
+n = 5
+print("Fibonacci value at position", n, "is", fibonacci(n))
